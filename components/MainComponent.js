6d1ed462a8448c7227ffa0c63ffd6c1b13fb2b00
Kenneth Smalls <kennethsmalls244@gmail.com>
2021-05-01 22:00:46 -0400

    Week 4 Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -323,17 +323,7 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
-
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
+        this.showNetInfo();
     }
 
     componentWillUnmount() {
@@ -361,7 +351,18 @@ class Main extends Component {
             : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
     }
 
-    
+    showNetInfo = async () => {
+       await NetInfo.fetch().then(connectionInfo => {
+            (Platform.OS === 'ios')
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
+        });
+
+        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+            this.handleConnectivityChange(connectionInfo);
+        });
+    }
 
     render() {
         return (
